35b96dbc88c49bd0818b9463a1754d76668ab155
prem sargara <premraju943@gmail.com>
2024-03-21 15:44:19 +0000

    update

--- food-website/models/Food.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const foodSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number
-});
-
-module.exports = mongoose.model('Food', foodSchema);
